9aaa6f3aa28aea7e1bff89cec25b67f905b19585
ljp2 <ljp2@lehigh.edu>
2023-04-08 05:57:54 -0400

    added DST to getHistoricalBars

--- Prepare/getHistoricalBars.py
+++ Prepare/getHistoricalBars.py
@@ -8,8 +8,14 @@ from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from alpaca.trading.client import TradingClient
 
+eastern_timezone= pytz.timezone('US/Eastern')
+
 apikey = 'PKKJZYB9P6Q36H84YMXF'
 secretkey = 'hBnEAEiD1f67p6hM4DKkUBtixY01YulNWSuGHOyx'
+stock_hist_client = StockHistoricalDataClient(apikey, secretkey)
+
+def isDST(date:datetime):
+    return eastern_timezone.localize(date).dst() != timedelta(0)
 
 def getBarsDirectory():
     if os.name == "nt":
@@ -20,9 +26,13 @@ def getBarsDirectory():
 
 
 def get_most_current_file_date(dir_name):
-    s = max(os.listdir(dir_name)).split('.')[0]
-    d = datetime.strptime(s, '%Y%m%d')
-    return d.strftime("%Y-%m-%d")
+    files = os.listdir(dir_name)
+    if len(files) == 0:
+        return 'EMPTY'
+    else:
+        s = max(os.listdir(dir_name)).split('.')[0]
+        d = datetime.strptime(s, '%Y%m%d')
+        return d.strftime("%Y-%m-%d")
 
 
 BARS1_DIRECTORY = getBarsDirectory()
@@ -37,28 +47,36 @@ if today.time() < time(18, 0):
 else:
     startday = today.date()
 
-query_time_start = datetime(startday.year, startday.month, startday.day, 14, 30)
+query_date_start = datetime(startday.year, startday.month, startday.day)
 most_current_file_date = get_most_current_file_date(BARS1_DIRECTORY)
 
-for i in range(300):
-    queryTime = query_time_start - i * dd
-    if queryTime.strftime("%Y-%m-%d") == most_current_file_date:
+for i in range(100):
+    queryDate = query_date_start - i * dd
+    if queryDate.strftime("%Y-%m-%d") == most_current_file_date:
         break
 
-    weekday = queryTime.weekday()
+    weekday = queryDate.weekday()
     if weekday <= 4:
-        print(queryTime, weekday,
+        print(queryDate, weekday,
               ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][weekday])
 
-        request_params = StockBarsRequest(
-            symbol_or_symbols='SPY',
-            timeframe=TimeFrame.Minute,
-            start=queryTime.strftime("%Y-%m-%d 14:30"),
-            end=queryTime.strftime("%Y-%m-%d 20:59")
-        )
+        if isDST(queryDate):
+            request_params = StockBarsRequest(
+                symbol_or_symbols='SPY',
+                timeframe=TimeFrame.Minute,
+                start=queryDate.strftime("%Y-%m-%d 13:30"),
+                end=queryDate.strftime("%Y-%m-%d 19:59")
+            )
+        else:
+             request_params = StockBarsRequest(
+                symbol_or_symbols='SPY',
+                timeframe=TimeFrame.Minute,
+                start=queryDate.strftime("%Y-%m-%d 14:30"),
+                end=queryDate.strftime("%Y-%m-%d 20:59")
+            )           
         try:
             bars = stock_hist_client.get_stock_bars(request_params)['SPY']
-            fname = f'{queryTime.strftime("%Y%m%d")}.csv'
+            fname = f'{queryDate.strftime("%Y%m%d")}.csv'
             f = open(f'{BARS1_DIRECTORY}/{fname}', 'w')
             f.write("time,open,high,low,close,volume,wap\n")
             for b in bars:
